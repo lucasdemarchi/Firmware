a888db4f39800e48079679b7b313e84d66cd6308
Lucas De Marchi <lucas.demarchi@intel.com>
2017-01-06 17:56:02 -0800

    aerofc: add pwm params and logic
    Respect PWM_MIN, PWM_MAX and similar parameters.

--- src/drivers/aerofc_pwmout/aerofc_pwmout.cpp
+++ src/drivers/aerofc_pwmout/aerofc_pwmout.cpp
@@ -37,6 +37,7 @@
 #include <px4_getopt.h>
 #include <px4_posix.h>
 #include <errno.h>
+#include <float.h>
 #include <termios.h>
 #include <cmath>
 
@@ -60,71 +61,87 @@
  */
 
 #define AEROFC_PWMOUT_MAX_MOTOR_NUM 16
+#define AEROFC_PWMOUT_DEVICE_PATH "/dev/aerofc_pwmout"
 
 static int _uart_fd = -1;
 class AEROFC_PWMOUT : public device::CDev
 {
 public:
-	AEROFC_PWMOUT(int channels_count);
+	AEROFC_PWMOUT(int channels_count, uint16_t pwm_disarmed,
+		      uint16_t pwm_min, uint16_t pwm_max);
 	virtual ~AEROFC_PWMOUT();
 	virtual int ioctl(file *filp, int cmd, unsigned long arg);
 	void cycle();
 
 private:
-	const uint16_t RPMSTOPPED = 800;
-	const uint16_t RPMMAX = 2300;
-
-	bool _is_armed;
-
-	unsigned	_poll_fds_num;
-	// subscriptions
-	int	_armed_sub;
-	int _test_motor_sub;
-	orb_advert_t        	_outputs_pub = nullptr;
-	actuator_outputs_s      _outputs;
+	static const unsigned _max_actuators = AEROFC_PWMOUT_MAX_MOTOR_NUM;
 	static actuator_armed_s	_armed;
+	static pwm_limit_t _pwm_limit;
 
-	// It needs to support the numbe of ESC
-	int	_control_subs[actuator_controls_s::NUM_ACTUATOR_CONTROL_GROUPS];
+	bool _pwm_on = false;
+	bool _throttle_armed = false;
 
-	pollfd	_poll_fds[actuator_controls_s::NUM_ACTUATOR_CONTROL_GROUPS];
+	unsigned _poll_fds_num;
+
+	float _mot_t_max = 0.0f;	// maximum rise time for motor (slew rate limiting)
+	float _thr_mdl_fac = 0.0f;	// thrust to pwm modelling factor
+
+	// subscriptions
+	int _armed_sub;
+	int _test_motor_sub;
+	orb_advert_t _outputs_pub = nullptr;
+	actuator_outputs_s _outputs;
+
+	// It needs to support the number of ESCs
+	int _control_subs[actuator_controls_s::NUM_ACTUATOR_CONTROL_GROUPS];
+
+	pollfd _poll_fds[actuator_controls_s::NUM_ACTUATOR_CONTROL_GROUPS];
 
 	actuator_controls_s _controls[actuator_controls_s::NUM_ACTUATOR_CONTROL_GROUPS];
 
-	orb_id_t	_control_topics[actuator_controls_s::NUM_ACTUATOR_CONTROL_GROUPS];
+	orb_id_t _control_topics[actuator_controls_s::NUM_ACTUATOR_CONTROL_GROUPS];
 
-	orb_advert_t      _to_mixer_status; 	///< mixer status flags
-	uint8_t           _channels_count; // The number of ESC channels
+	uint16_t _disarmed_pwm[AEROFC_PWMOUT_MAX_MOTOR_NUM];
+	uint16_t _min_pwm[AEROFC_PWMOUT_MAX_MOTOR_NUM];
+	uint16_t _max_pwm[AEROFC_PWMOUT_MAX_MOTOR_NUM];
 
-	MixerGroup	*_mixers;
-	uint32_t	_groups_required;
-	uint32_t	_groups_subscribed;
-	volatile bool	_initialized;
-	unsigned	_pwm_default_rate;
-	unsigned	_current_update_rate;
-	void		subscribe();
+	orb_advert_t _to_mixer_status; ///< mixer status flags
+	uint8_t _channels_count; // The number of ESC channels
 
-	void		work_stop();
+	hrt_abstime _time_last_mix = 0;
+
+	MixerGroup *_mixers;
+	uint32_t _groups_required;
+	uint32_t _groups_subscribed;
+	volatile bool _initialized;
+	unsigned _pwm_default_rate = 100;
+
+	void subscribe();
+	void unsubscribe();
 	uint16_t crc16(uint8_t *p, uint8_t len);
-	void send_esc_outputs(const float *pwm, const unsigned num_pwm);
-	static int control_callback(uintptr_t handle,
-				    uint8_t control_group,
-				    uint8_t control_index,
-				    float &input);
+
+	void send_esc_outputs(const uint16_t *pwm, const unsigned num_pwm);
+
+	static int control_callback(uintptr_t handle, uint8_t control_group,
+				    uint8_t control_index, float &input);
+
+	static bool arm_nothrottle()
+	{
+		return ((_armed.prearmed && !_armed.armed) || _armed.in_esc_calibration_mode);
+	}
 };
 
 actuator_armed_s AEROFC_PWMOUT::_armed = {};
+pwm_limit_t AEROFC_PWMOUT::_pwm_limit;
 
 namespace
 {
-AEROFC_PWMOUT	*aerofc_pwmout = nullptr;
+AEROFC_PWMOUT *aerofc_pwmout = nullptr;
 }
 
-# define AEROFC_PWMOUT_DEVICE_PATH	"/dev/aerofc_pwmout"
-
-AEROFC_PWMOUT::AEROFC_PWMOUT(int channels_count):
+AEROFC_PWMOUT::AEROFC_PWMOUT(int channels_count, uint16_t pwm_disarmed,
+			     uint16_t pwm_min, uint16_t pwm_max) :
 	CDev("aerofc_pwmout", AEROFC_PWMOUT_DEVICE_PATH),
-	_is_armed(false),
 	_poll_fds_num(0),
 	_armed_sub(-1),
 	_test_motor_sub(-1),
@@ -135,10 +152,7 @@ AEROFC_PWMOUT::AEROFC_PWMOUT(int channels_count):
 	_mixers(nullptr),
 	_groups_required(0),
 	_groups_subscribed(0),
-	_initialized(false),
-	_pwm_default_rate(400),
-	_current_update_rate(0)
-
+	_initialized(false)
 {
 	_control_topics[0] = ORB_ID(actuator_controls_0);
 	_control_topics[1] = ORB_ID(actuator_controls_1);
@@ -151,6 +165,12 @@ AEROFC_PWMOUT::AEROFC_PWMOUT(int channels_count):
 		_outputs.output[i] = NAN;
 	}
 
+	for (unsigned i = 0; i < _max_actuators; i++) {
+		_min_pwm[i] = pwm_min;
+		_max_pwm[i] = pwm_max;
+		_disarmed_pwm[i] = pwm_disarmed;
+	}
+
 	_outputs.noutputs = 0;
 }
 
@@ -158,7 +178,7 @@ AEROFC_PWMOUT::~AEROFC_PWMOUT()
 {
 	if (_initialized) {
 		/* tell the task we want it to go away */
-		work_stop();
+		unsubscribe();
 
 		int i = 10;
 
@@ -169,9 +189,6 @@ AEROFC_PWMOUT::~AEROFC_PWMOUT()
 
 		} while (_initialized && i > 0);
 	}
-
-	// clean up the alternate device node
-	//unregister_class_devname(PWM_OUTPUT_BASE_DEVICE_PATH, _class_instance);
 
 	aerofc_pwmout = nullptr;
 }
@@ -225,7 +242,7 @@ uint16_t AEROFC_PWMOUT::crc16(uint8_t *p, uint8_t len)
 	return crc;
 }
 
-void AEROFC_PWMOUT::send_esc_outputs(const float *pwm, const unsigned num_pwm)
+void AEROFC_PWMOUT::send_esc_outputs(const uint16_t *pwm, const unsigned num_pwm)
 {
 #pragma pack(push,1)
 	struct {
@@ -240,17 +257,17 @@ void AEROFC_PWMOUT::send_esc_outputs(const float *pwm, const unsigned num_pwm)
 	packet.magic = 0xA2;
 
 	for (uint8_t i = 0; i < num_pwm; i++) {
-		if (pwm[i] > RPMMAX) {
-			packet.ch[i] = RPMMAX;
+		if (pwm[i] < 800) {
+			packet.ch[i] = 0;
 
-		} else if (pwm[i] < RPMSTOPPED) {
-			packet.ch[i] = RPMSTOPPED;
+		} else if (pwm[i] >= 2200) {
+			packet.ch[i] = 0xFFF;
 
 		} else {
-			packet.ch[i] = pwm[i];
+			packet.ch[i] = (pwm[i] - 800) * 4095 / 1400;
 		}
 
-		packet.ch[i] = __builtin_bswap16(packet.ch[i]);
+		packet.ch[i] = __builtin_bswap16((uint16_t) packet.ch[i]);
 	}
 
 	packet.crc = crc16((uint8_t *) &packet, sizeof(packet) - sizeof(packet.crc));
@@ -266,55 +283,53 @@ void AEROFC_PWMOUT::send_esc_outputs(const float *pwm, const unsigned num_pwm)
 void AEROFC_PWMOUT::cycle()
 {
 	if (!_initialized) {
-		_current_update_rate = 0;
-		/* advertise the mixed control outputs, insist on the first group output */
-		_outputs_pub = orb_advertise(ORB_ID(actuator_outputs), &_outputs);
-		multirotor_motor_limits_s multirotor_motor_limits = {};
-		_to_mixer_status = orb_advertise(ORB_ID(multirotor_motor_limits), &multirotor_motor_limits);
 		_armed_sub = orb_subscribe(ORB_ID(actuator_armed));
 		_test_motor_sub = orb_subscribe(ORB_ID(test_motor));
+
+		/* initialize PWM limit lib */
+		pwm_limit_init(&_pwm_limit);
+
+		/* advertise the mixed control outputs, insist on the first group output */
+		_outputs_pub = orb_advertise(ORB_ID(actuator_outputs), &_outputs);
+
+		multirotor_motor_limits_s multirotor_motor_limits = {};
+		_to_mixer_status = orb_advertise(ORB_ID(multirotor_motor_limits), &multirotor_motor_limits);
+
+		param_t param_handle;
+		// maximum motor slew rate parameter
+		param_handle = param_find("MOT_SLEW_MAX");
+
+		if (param_handle != PARAM_INVALID) {
+			param_get(param_handle, &_mot_t_max);
+		}
+
+		// thrust to pwm modelling factor
+		param_handle = param_find("THR_MDL_FAC");
+
+		if (param_handle != PARAM_INVALID) {
+			param_get(param_handle, &_thr_mdl_fac);
+		}
+
+		int update_rate_msec = int(1000 / _pwm_default_rate);
+
+		DEVICE_DEBUG("adjusted actuator update interval to %ums", update_rate_msec);
+
+		for (unsigned i = 0; i < actuator_controls_s::NUM_ACTUATOR_CONTROL_GROUPS; i++) {
+			if (_control_subs[i] > 0) {
+				orb_set_interval(_control_subs[i], update_rate_msec);
+			}
+		}
+
 		_initialized = true;
 	}
 
 	if (_groups_subscribed != _groups_required) {
 		subscribe();
 		_groups_subscribed = _groups_required;
-		_current_update_rate = 0;
 	}
-
-	unsigned max_rate = _pwm_default_rate ;
-
-	if (_current_update_rate != max_rate) {
-		_current_update_rate = max_rate;
-		int update_rate_in_ms = int(1000 / _current_update_rate);
-
-		/* reject faster than 500 Hz updates */
-		if (update_rate_in_ms < 2) {
-			update_rate_in_ms = 2;
-		}
-
-		/* reject slower than 10 Hz updates */
-		if (update_rate_in_ms > 100) {
-			update_rate_in_ms = 100;
-		}
-
-		DEVICE_DEBUG("adjusted actuator update interval to %ums", update_rate_in_ms);
-
-		for (unsigned i = 0; i < actuator_controls_s::NUM_ACTUATOR_CONTROL_GROUPS; i++) {
-			if (_control_subs[i] > 0) {
-				orb_set_interval(_control_subs[i], update_rate_in_ms);
-			}
-		}
-
-		// set to current max rate, even if we are actually checking slower/faster
-		_current_update_rate = max_rate;
-	}
-
-
 
 	/* check if anything updated */
 	int ret = ::poll(_poll_fds, _poll_fds_num, 5);
-
 
 	/* this would be bad... */
 	if (ret < 0) {
@@ -337,9 +352,31 @@ void AEROFC_PWMOUT::cycle()
 		}
 
 		size_t num_outputs = _channels_count;
+		uint16_t pwm_limited[_max_actuators];
 
 		/* can we mix? */
-		if (_is_armed && _mixers != nullptr) {
+		if (_armed.armed && _mixers != nullptr) {
+			hrt_abstime now = hrt_absolute_time();
+			float dt = (now - _time_last_mix) / 1e6f;
+			_time_last_mix = now;
+
+			if (dt < 0.0001f) {
+				dt = 0.0001f;
+
+			} else if (dt > 0.02f) {
+				dt = 0.02f;
+			}
+
+			if (_mot_t_max > FLT_EPSILON) {
+				// maximum value the ouputs of the multirotor mixer are allowed to change in this cycle
+				// factor 2 is needed because actuator ouputs are in the range [-1,1]
+				float delta_out_max = 2.0f * 1000.0f * dt / (_max_pwm[0] - _min_pwm[0]) / _mot_t_max;
+				_mixers->set_max_delta_out_once(delta_out_max);
+			}
+
+			if (_thr_mdl_fac > FLT_EPSILON) {
+				_mixers->set_thrust_factor(_thr_mdl_fac);
+			}
 
 			/* do mixing */
 			num_outputs = _mixers->mix(&_outputs.output[0], num_outputs, NULL);
@@ -357,23 +394,15 @@ void AEROFC_PWMOUT::cycle()
 				_outputs.output[i] = NAN;
 			}
 
-			/* iterate actuators */
-			for (unsigned i = 0; i < num_outputs; i++) {
-				/* last resort: catch NaN, INF and out-of-band errors */
-				if (i < _outputs.noutputs &&
-				    PX4_ISFINITE(_outputs.output[i]) &&
-				    _outputs.output[i] >= -1.0f &&
-				    _outputs.output[i] <= 1.0f) {
-					/* scale for PWM output 1250 - 1950us */
-					_outputs.output[i] = 1600 + (350 * _outputs.output[i]);
+			/* the PWM limit call takes care of out of band errors, NaN and constrains */
+			pwm_limit_calc(_throttle_armed, arm_nothrottle(), num_outputs, 0,
+				       _disarmed_pwm, _min_pwm, _max_pwm, _outputs.output,
+				       pwm_limited, &_pwm_limit);
 
-				} else {
-					/*
-					 * Value is NaN, INF or out of band - set to the minimum value.
-					 * This will be clearly visible on the servo status and will limit the risk of accidentally
-					 * spinning motors. It would be deadly in flight.
-					 */
-					_outputs.output[i] = RPMSTOPPED;
+			/* overwrite outputs in case of lockdown with disarmed PWM values */
+			if (_armed.lockdown || _armed.manual_lockdown) {
+				for (size_t i = 0; i < num_outputs; i++) {
+					pwm_limited[i] = _disarmed_pwm[i];
 				}
 			}
 
@@ -389,15 +418,18 @@ void AEROFC_PWMOUT::cycle()
 			if (test_motor_updated) {
 				struct test_motor_s test_motor;
 				orb_copy(ORB_ID(test_motor), _test_motor_sub, &test_motor);
-				_outputs.output[test_motor.motor_number] = RPMSTOPPED + ((RPMMAX - RPMSTOPPED) * test_motor.value);
+				_outputs.output[test_motor.motor_number] = test_motor.value;
 				PX4_INFO("setting motor %i to %.1lf", test_motor.motor_number,
 					 (double)_outputs.output[test_motor.motor_number]);
 			}
 
-			/* set the invalid values to the minimum */
 			for (unsigned i = 0; i < num_outputs; i++) {
-				if (!PX4_ISFINITE(_outputs.output[i])) {
-					_outputs.output[i] = RPMSTOPPED;
+				if (PX4_ISFINITE(_outputs.output[i])) {
+					pwm_limited[i] = _min_pwm[i] + ((_max_pwm[i] - _min_pwm[i]) / 2) * _outputs.output[i];
+
+				} else {
+					/* set the invalid values to the minimum */
+					pwm_limited[i] = _disarmed_pwm[i];
 				}
 			}
 
@@ -405,18 +437,9 @@ void AEROFC_PWMOUT::cycle()
 			for (size_t i = num_outputs; i < sizeof(_outputs.output) / sizeof(_outputs.output[0]); i++) {
 				_outputs.output[i] = NAN;
 			}
-
 		}
 
-		const unsigned esc_count = num_outputs;
-		float motor_out[AEROFC_PWMOUT_MAX_MOTOR_NUM];
-
-		// Use the system defaults
-		for (int i = 0; i < esc_count; ++i) {
-			motor_out[i] = _outputs.output[i];
-		}
-
-		send_esc_outputs(motor_out, esc_count);
+		send_esc_outputs(pwm_limited, num_outputs);
 
 		/* and publish for anyone that cares to see */
 		orb_publish(ORB_ID(actuator_outputs), _outputs_pub, &_outputs);
@@ -424,28 +447,30 @@ void AEROFC_PWMOUT::cycle()
 	}
 
 	bool updated;
-
 	orb_check(_armed_sub, &updated);
 
 	if (updated) {
 		orb_copy(ORB_ID(actuator_armed), _armed_sub, &_armed);
 
-		if (_is_armed != _armed.armed) {
+		/* Update the armed status and check that we're not locked down.
+		 * We also need to arm throttle for the ESC calibration. */
+		_throttle_armed = (_armed.armed && !_armed.lockdown) ||
+				  (_armed.in_esc_calibration_mode);
+
+		bool pwm_on = _armed.armed || _armed.in_esc_calibration_mode;
+
+		if (_pwm_on != pwm_on) {
+			_pwm_on = pwm_on;
+
 			/* reset all outputs */
 			for (size_t i = 0; i < sizeof(_outputs.output) / sizeof(_outputs.output[0]); i++) {
 				_outputs.output[i] = NAN;
 			}
 		}
-
-		/* do not obey the lockdown value, as lockdown is for PWMSim */
-		_is_armed = _armed.armed;
-
 	}
-
-
 }
 
-void AEROFC_PWMOUT::work_stop()
+void AEROFC_PWMOUT::unsubscribe()
 {
 	for (unsigned i = 0; i < actuator_controls_s::NUM_ACTUATOR_CONTROL_GROUPS; i++) {
 		if (_control_subs[i] > 0) {
@@ -460,7 +485,6 @@ void AEROFC_PWMOUT::work_stop()
 	_test_motor_sub = -1;
 
 	DEVICE_LOG("stopping");
-	_initialized = false;
 }
 
 int
@@ -555,39 +579,25 @@ AEROFC_PWMOUT::ioctl(file *filp, int cmd, unsigned long arg)
 namespace aerofc_pwmout_drv
 {
 
-
-volatile bool _task_should_exit = false; // flag indicating if aerofc_pwmout task should exit
+static volatile bool _task_should_exit = false; // flag indicating if aerofc_pwmout task should exit
+static volatile bool _is_running = false;       // flag indicating if aerofc_pwmout app is running
+static px4_task_t _task_handle = -1;            // handle to the task main thread
 static char _device[32] = {};
-static bool _is_running = false;         // flag indicating if aerofc_pwmout app is running
-static px4_task_t _task_handle = -1;     // handle to the task main thread
 static int _supported_channel_count = 0;
 
-static bool _flow_control_enabled = false;
+// esc parameters
+static int32_t _pwm_disarmed;
+static int32_t _pwm_min;
+static int32_t _pwm_max;
 
-void usage();
-
-void start();
-void stop();
-int aerofc_pwmout_start(void);
-int aerofc_pwmout_stop(void);
-
-void task_main_trampoline(int argc, char *argv[]);
-
-void task_main(int argc, char *argv[]);
-
-int initialise_uart();
-
-int deinitialize_uart();
-
-int enable_flow_control(bool enabled);
-
-int aerofc_pwmout_start(void)
+static int aerofc_pwmout_start(void)
 {
 	int ret = OK;
 
 	if (aerofc_pwmout == nullptr) {
 
-		aerofc_pwmout = new AEROFC_PWMOUT(_supported_channel_count);
+		aerofc_pwmout = new AEROFC_PWMOUT(_supported_channel_count, _pwm_disarmed,
+						  _pwm_min, _pwm_max);
 
 		if (aerofc_pwmout == nullptr) {
 			ret = -ENOMEM;
@@ -606,12 +616,11 @@ int aerofc_pwmout_start(void)
 	return ret;
 }
 
-int aerofc_pwmout_stop(void)
+static int aerofc_pwmout_stop(void)
 {
 	int ret = OK;
 
 	if (aerofc_pwmout != nullptr) {
-
 		delete aerofc_pwmout;
 		aerofc_pwmout = nullptr;
 	}
@@ -619,7 +628,7 @@ int aerofc_pwmout_stop(void)
 	return ret;
 }
 
-int initialise_uart()
+static int initialise_uart()
 {
 	// open uart
 	_uart_fd = open(_device, O_RDWR | O_NOCTTY | O_NONBLOCK);
@@ -637,6 +646,8 @@ int initialise_uart()
 
 	// clear ONLCR flag (which appends a CR for every LF)
 	uart_config.c_oflag &= ~ONLCR;
+	// no flow control
+	uart_config.c_cflag &= ~CRTSCTS;
 
 	// set baud rate
 	if (cfsetispeed(&uart_config, speed) < 0 || cfsetospeed(&uart_config, speed) < 0) {
@@ -651,44 +662,16 @@ int initialise_uart()
 		return -1;
 	}
 
-	// setup output flow control
-	if (enable_flow_control(false)) {
-		PX4_WARN("hardware flow disable failed");
-	}
-
 	return _uart_fd;
 }
 
-int enable_flow_control(bool enabled)
-{
-	struct termios uart_config;
-
-	int ret = tcgetattr(_uart_fd, &uart_config);
-
-	if (enabled) {
-		uart_config.c_cflag |= CRTSCTS;
-
-	} else {
-		uart_config.c_cflag &= ~CRTSCTS;
-	}
-
-	ret = tcsetattr(_uart_fd, TCSANOW, &uart_config);
-
-	if (!ret) {
-		_flow_control_enabled = enabled;
-	}
-
-	return ret;
-}
-
-int deinitialize_uart()
+static int deinitialize_uart()
 {
 	return close(_uart_fd);
 }
 
-void task_main(int argc, char *argv[])
+static void task_main(int argc, char *argv[])
 {
-
 	_is_running = true;
 
 	if (initialise_uart() < 0) {
@@ -720,16 +703,21 @@ void task_main(int argc, char *argv[])
 	_is_running = false;
 }
 
-void task_main_trampoline(int argc, char *argv[])
+static void task_main_trampoline(int argc, char *argv[])
 {
 	task_main(argc, argv);
 }
 
-void start()
+static void start()
 {
 	ASSERT(_task_handle == -1);
 
 	_task_should_exit = false;
+
+	// gets the parameters for the esc's pwm
+	param_get(param_find("PWM_DISARMED"), &_pwm_disarmed);
+	param_get(param_find("PWM_MIN"), &_pwm_min);
+	param_get(param_find("PWM_MAX"), &_pwm_max);
 
 	/* start the task */
 	_task_handle = px4_task_spawn_cmd("aerofc_pwmout_main",
@@ -746,7 +734,7 @@ void start()
 	}
 }
 
-void stop()
+static void stop()
 {
 	_task_should_exit = true;
 
@@ -760,14 +748,14 @@ void stop()
 	_task_handle = -1;
 }
 
-void usage()
+static void usage()
 {
-	PX4_INFO("usage: aerofc_pwmout start -d /dev/ttyS2 -n <1-8>");
+	PX4_INFO("usage: aerofc_pwmout start -d /dev/ttyS2 -n <1-16>");
 	PX4_INFO("       aerofc_pwmout stop");
 	PX4_INFO("       aerofc_pwmout status");
 }
 
-} // namespace aerofc_pwmout
+}
 
 // driver 'main' command
 extern "C" __EXPORT int aerofc_pwmout_main(int argc, char *argv[]);
